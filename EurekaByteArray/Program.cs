04cea10497a277974e538553c67c1298287e474f
Bartek <bartlomiej.kacki@gmail.com>
2024-11-06 19:40:44 +0100

    Add project files.

---
+++ EurekaByteArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Text;
+
+namespace EurekaByteArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("eureka.txt", "Eureka!", Encoding.Unicode);
+            byte[] eurekaBytes = File.ReadAllBytes("eureka.txt");
+            foreach(byte b in eurekaBytes)
+                Console.Write($"{b}".PadLeft(5));
+            Console.WriteLine();
+            foreach(byte b in eurekaBytes)
+                Console.Write($"{b:x2}".PadLeft(5));
+            Console.WriteLine();
+            Console.WriteLine(Encoding.Unicode.GetString(eurekaBytes, 2, eurekaBytes.Length-2));
+        }
+    }
+}
